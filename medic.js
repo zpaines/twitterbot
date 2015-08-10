90cb197dd47dd8f106366479fb9e2c9a774bde84
Thomas Schaffner <t.f.schaffner@gmail.com>
2015-08-09 20:45:06 -0400

    Filename tweaking

--- medic.js
+++ medic.js
@@ -1,6 +1,39 @@
+var sha512 = require('js-sha512');
+
 var exports = module.exports = {};
 
 exports.sanitize = function (text) {
 	var match = /[<>;&\*\\/\^_~()]/gi;
 	return text.replace(match, '');
+}
+
+exports.hashPass = function (password) {
+	return sha512(password);
+}
+
+exports.validateUser = function (user, password) {
+	return (sha512(password) == user.hashedPass);
+}
+
+exports.checkKeys = function (body, fields) {
+	var errorMessage = '';
+	var key = '';
+
+	// Ensure all fields exist
+	for (var i = 0; i < fields.length; i++) {
+		key = fields[i];
+		if (!(key in body)) {
+			errorMessage += key + ' missing!;';
+		}
+	}
+
+	return errorMessage;
+}
+
+exports.requireAuth = function (req, res, next) {
+	if (req.isAuthenticated() != true) {
+		res.render('signin', { isAuthenticated: req.isAuthenticated() });
+	} else {
+		next();
+	}
 }

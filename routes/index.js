1bc313d9235229add04cab80e604bb12d3d21690
Thomas Schaffner <t.f.schaffner@gmail.com>
2015-08-28 18:12:08 -0400

    Lots of updates. Ask me questions

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var photoPath = "uploads/";
 var photoURL = "/picture/";
 var upload = multer({dest: photoPath});
 var path = require('path');
+var randomstring = require('randomstring');
 
 var medic = require('../medic.js');
 
@@ -24,15 +25,37 @@ router.get(photoURL + ':filePath', function(req,res) {
 router.post('/upload', upload.single('file'), function(req, res) {});
 
 router.get('/guidelist', function(req,res) {
-   var db = req.db;
-   var collection = db.get('guides');
-   collection.find({},{},function(e,docs) {
-       res.json(docs);
-   });
+  var db = req.db;
+  var collection = db.get('guides');
+  collection.find({},{},function(e,docs) {
+    if (e) { return res.status(500).send({error: "Error with lookup"}); }
+
+    var toReturn = [];
+
+    for (var i = 0; i < docs.length; i++) {
+      var doc = docs[i];
+      var newGuide = {
+        name: medic.sanitize(doc.name),
+        email: medic.sanitize(doc.email),
+        major: medic.sanitize(doc.major),
+        language: medic.sanitize(doc.language),
+        photoPath: medic.sanitize(doc.photoPath)
+      }
+      toReturn.push(newGuide);
+    }
+    return res.status(200).send(toReturn);
+  });
 });
 
 router.get('/profile', medic.requireAuth, function(req, res) {
-  res.render('profile', {name: req.user.name});
+  res.render('profile', {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+    major: req.user.major,
+    language: req.user.language,
+    photoPath: req.user.photoPath
+  });
 });
 
 router.get('/newguide', function(req, res) {
@@ -56,8 +79,10 @@ router.post('/timeslot', medic.requireAuth, function(req, res) {
     time: medic.sanitize(req.body.time),
     duration: medic.sanitize(req.body.duration),
     guideEmail: req.user.email,
-    guideID: req.user._id
+    randomID: medic.sanitize(randomstring.generate(35))
   }
+
+  // Hoping that randomstring doesn't collide I guess
 
   var db = req.db;
   var timeslots = db.get('timeslots');
@@ -75,7 +100,23 @@ router.get('/times', function(req, res) {
   timeslots = req.db.get('timeslots');
   timeslots.find({},{},function(err, docs) {
     if (!err) {
-      res.json(docs);
+      var toReturn = [];
+
+      for (var i = 0; i < docs.length; i++) {
+        var doc = docs[i];
+
+        var newTime = {
+          date: medic.sanitize(doc.date),
+          time: medic.sanitize(doc.time),
+          duration: medic.sanitize(doc.duration),
+          guideEmail: medic.sanitize(doc.guideEmail),
+          randomID: medic.sanitize(doc.randomID)
+        }
+
+        toReturn.push(newTime);
+      }
+
+      return res.status(200).send(toReturn);
     } else {
       res.send(err);
     }
@@ -100,20 +141,6 @@ router.get('/guidetimes', function(req, res) {
         return res.status(200).send(docs);
       }
     });
-
-  // TODO(tfs): Query by guideID currently not functioning properly.
-
-  // } else if ('guideID' in req.query) {
-  //   var cleanID = medic.sanitize(req.query.guideID);
-
-  //   timeslots.find({guideID: cleanID}, {}, function (err, docs) {
-  //     if (err) {
-  //       return res.status(500).send({error:'Lookup failed'});
-  //     } else {
-  //       return res.status(200).send(docs);
-  //     }
-  //   });
-
   } else {
     timeslots.find({}, {}, function (err, docs) {
       if (err) {
@@ -142,26 +169,27 @@ router.post('/appointment', function (req, res) {
   var timeslots = req.db.get('timeslots');
   var guides = req.db.get('guides');
 
-  timeslots.find({_id: medic.sanitize(req.body.slotID)}, {}, function (err, slotResults) {
+  timeslots.find({randomID: medic.sanitize(req.body.slotID)}, {}, function (err, slotResults) {
     if ((err) || (slotResults.length != 1)) { return res.status(500).send({error:'Lookup of timeslot failed'}); }
     slot = slotResults[0];
 
-    guides.find({_id: medic.sanitize(String(slot.guideID))}, {}, function (er, guideResults) {
+    guides.find({email: medic.sanitize(String(slot.guideEmail))}, {}, function (er, guideResults) {
       if ((err) || (guideResults.length != 1)) { return res.status(500).send({error:'Lookup of guide failed'}); }
       guide = guideResults[0];
 
-      console.log(slot._id);
-      var cleanSlotID = medic.sanitize(String(slot._id));
-      var cleanGuideID = medic.sanitize(String(guide._id));
+      var cleanSlotID = medic.sanitize(String(slot.randomID));
       var cleanGuideEmail = medic.sanitize(String(guide.email));
       var cleanEmail = medic.sanitize(String(req.body.responseEmail));
+      var cleanRandomID = medic.sanitize(medic.hashOther(randomstring.generate(40)));
 
       var newApt = {
         timeslotID: cleanSlotID,
-        guideID: cleanGuideID,
         guideEmail: cleanGuideEmail,
-        responseEmail: cleanEmail
+        responseEmail: cleanEmail,
+        randomID: cleanRandomID
       }
+
+      // Send email yay
 
       apts.insert(newApt, function (e, inserted) {
         if (e) { return res.status(500).send({error:'Failed to save appointment'}); }
@@ -171,16 +199,71 @@ router.post('/appointment', function (req, res) {
   });
 });
 
+router.get('/appointment/:id', function (req, res) {
+  var db = req.db;
+  var appointments = db.get('appointments');
+
+  var cleanID = medic.sanitize(req.params.id);
+
+  appointments.find({randomID: cleanID}, function (err, docs) {
+    if (err) { return res.status(500).send({error: "Error with appointment lookup"}); }
+
+    var toReturn = [];
+
+    for (var i = 0; i < docs.length; i++) {
+      var doc = docs[i];
+
+      var newApt = {
+        guideID: medic.sanitize(doc.guideID),
+        guideEmail: medic.sanitize(doc.guideEmail),
+        responseEmail: medic.sanitize(doc.responseEmail),
+        randomID: medic.sanitize(doc.randomID)
+      }
+
+      toReturn.push(newApt);
+    }
+
+    return res.status(200).send(toReturn);
+  });
+});
+
+router.get('/appointments', medic.requireAuth, function (req, res) {
+  var db = req.db;
+  var appointments = db.get('appointments');
+
+  var cleanEmail = medic.sanitize(req.user.email);
+
+  var toReturn = []
+
+  appointments.find({guideEmail: cleanEmail}, function (err, docs) {
+    if (err) { return res.status(500).send({error: "Error looking up appointments"}); }
+
+    for (var i = 0; i < docs.length; i++) {
+      var doc = docs[i];
+
+      var newApt = {
+        timeslotID: medic.sanitize(doc.timeslotID),
+        guideEmail: medic.sanitize(doc.guideEmail),
+        responseEmail: medic.sanitize(doc.responseEmail),
+        randomID: medic.sanitize(doc.randomID)
+      }
+
+      toReturn.push(newApt);
+    }
+
+    return res.status(200).send(toReturn);
+  });
+});
+
 router.delete('/appointment/:id', function (req, res) {
   var db = req.db;
   var appointments = db.get('appointments');
 
   var cleanID = medic.sanitize(req.params.id);
 
-  appointments.remove({_id: cleanID}, function (err, records) {
+  appointments.remove({randomID: cleanID}, function (err, records) {
     if (err) { return res.status(500).send({error: "Error with appointment lookup"}); }
-    console.log(records);
-    res.status(200).send('OK');
+    return res.status(200).send('OK');
   });
 });
 

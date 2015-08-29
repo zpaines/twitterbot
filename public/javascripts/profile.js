22da4c4176f24b546c433c9447996f99acd3dbc6
Zach <zacharysabin+github@gmail.com>
2015-08-28 21:31:08 -0400

    Profiles started

---
+++ public/javascripts/profile.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    console.log("test");
+    var panels = $('.user-infos');
+    var panelsButton = $('.dropdown-user');
+    panels.hide();
+
+    var request = $.ajax({
+       type: "GET",
+       url: "/guidetimes",
+       data: {"guideEmail":"zpaines1@jhu.edu"}, // serializes the form's elements.
+     });
+    request.success(function(jqXHR, textStatus) {
+        console.log("Test");
+        $.each(request.responseJSON, function(index, value) {
+            console.log(value);
+            $('timeslotModalBody').append('<p name="slotID">' + value.time + ' on ' + value.dateString + '</p>')
+        });
+    });
+
+    //Click dropdown
+    panelsButton.click(function() {
+        //get data-for attribute
+        var dataFor = $(this).attr('data-for');
+        var idFor = $(dataFor);
+
+        //current button
+        var currentButton = $(this);
+        idFor.slideToggle(400, function() {
+            //Completed slidetoggle
+            if(idFor.is(':visible'))
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
+            }
+            else
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
+            }
+        })
+    });
+
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('button').click(function(e) {
+        e.preventDefault();
+        alert("This is a demo.\n :-)");
+    });
+});
